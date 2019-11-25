bc29db2583ee735d72e30d4d84bfec376d38a52b
League Student <league@iMac-11.attlocal.net>
2019-11-25 15:27:38 -0800

    robot

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,9 +7,21 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
+		Robot rob = new Robot ();
+	rob.sparkle();
+	rob.penDown();
+	rob.setSpeed(100);
+	for(int i=0;i<4;i++){
+	rob.move(100);
+	rob.turn(90);
+	}
 	
-		
+	rob.penUp();
+	rob.move(200);
+	rob.hide();
+	rob.show();
+	rob.turn(360);
+			
 		
 	}
 }

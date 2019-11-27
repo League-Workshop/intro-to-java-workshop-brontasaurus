a796ca84d4e9cd571d25fa300be81859f9f1334f
League Student <league@iMac-11.attlocal.net>
2019-11-27 14:34:14 -0800

    day3

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,16 +9,43 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak("spell mandlebrot");
 		// 2. Catch the user's answer in a String
-
+String name = JOptionPane.showInputDialog("type here");
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if(name.equalsIgnoreCase("mandlebrot")) {
 		// 4. Otherwise say "wrong"
+	speak("correct");
+}
+else {
+	speak("wrong");
+}
 
 		// 5. repeat the process for other words
-		
+speak("spell des oiseaux");
+// 2. Catch the user's answer in a String
+name = JOptionPane.showInputDialog("type here");
+// 3. If the user spelled the word correctly, speak "correct"
+if(name.equalsIgnoreCase("des oiseaux")) {
+// 4. Otherwise say "wrong"
+speak("correct");
+}
+else {
+speak("wrong");
+}
+
+	speak("spell gurgle");
+	// 2. Catch the user's answer in a String
+	name = JOptionPane.showInputDialog("type here");
+	// 3. If the user spelled the word correctly, speak "correct"
+	if(name.equalsIgnoreCase("gurgle")) {
+	// 4. Otherwise say "wrong"
+	speak("correct");
 	}
+	else {
+	speak("wrong");
+	}
+		}
 
 	static void speak(String words) {
 		try {
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,37 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String name = JOptionPane.showInputDialog("what is your name?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(name.equalsIgnoreCase("arthur king of the britians")) {
 		// 4.  if the user's answer was correct, add one to their score 
-		
+		score++;
+		}
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		name = JOptionPane.showInputDialog("what is your quest?");
+		
+		if(name.equalsIgnoreCase("I seek the holy grail")) {
+			score++;
+		}
+		name = JOptionPane.showInputDialog("what is the maximum velosity of an unlaiden swallow?");
+		
+		if(name.equalsIgnoreCase("which one? African or european swallow?")) {
+			score++;
+		}
+		
+name = JOptionPane.showInputDialog("I dont know that! AHHHH!");
+		
+			score++;
 		
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		System.out.println(score);
 	}
 }
